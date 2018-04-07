7ccf32fd5abe18abf104d5442e182e61fd554391
Maken Hubbard <makhub26@outlook.com>
2018-04-07 12:50:33 -0500

    adding the final touches to the language translator

--- main.js
+++ main.js
@@ -4,21 +4,21 @@
 
 
 const chinese = {
-    "merry":"快活",
-    "christmas":"聖誕",
+    "merry": "快活",
+    "christmas": "聖誕",
     "and": "和",
-    "happy": "快樂", 
-    "new" :"新",
-    "year":  "年",
+    "happy": "快樂",
+    "new": "新",
+    "year": "年"
 }
 
 const french = {
     "merry": "joyeux",
-    "christmas" : "Noël",
-    "and" : "et",
+    "christmas": "Noël",
+    "and": "et",
     "happy": "content",
     "new": "Nouveau",
-    "year": "an",
+    "year": "an"
 }
 
 const spanish = {
@@ -27,51 +27,69 @@ const spanish = {
     "and": "y",
     "happy": "contento",
     "new": "nuevo",
-    "year": "año",
+    "year": "año"
 }
 
 const inputSpot = document.getElementById('inputText');
 const chineseButt = document.getElementById('chinese-btn');
 const frenchButt = document.getElementById('french-btn');
-const spanishButt = document.getElementById('spanish-btn'); 
+const spanishButt = document.getElementById('spanish-btn');
 const outputSpot = document.getElementById('outputText');
 
 chineseButt.addEventListener('click', (e) => {
-    console.log("my chinese event", e);
-    const userInput = inputSpot.value.toLowerCase();
-    let domOutput = chineseButt[userInput];
-    outputSpot.innerHTML = domOutput 
+    const userInput = inputSpot.value.toLowerCase().split(' ');
+    let domOutput = '';
+    userInput.forEach(word => {
+        if (chinese[word]) {
+            domOutput += " " + chinese[word];
+        } else {
+            domOutput = "We dont know a whole lot of this language";
+        }
+    });
+    outputSpot.innerHTML = domOutput;
 })
 
 frenchButt.addEventListener('click', (e) => {
-    console.log("my french event", e);
-    const userInput = inputSpot.value.toLowerCase()
-    let domOutput = frenchButt[userInput];
+    const userInput = inputSpot.value.toLowerCase().split(' ');
+    let domOutput = '';
+    userInput.forEach(word => {
+        if (french[word]) {
+            domOutput += " " + (french[word]);
+        } else {
+            domOutput = "We dont know a whole lot of this language";
+        }
+    })
     outputSpot.innerHTML = domOutput;
 })
 
 spanishButt.addEventListener('click', (e) => {
-    console.log("my spanish event", e);
-    const userInput = inputSpot.value.toLowerCase();
-    let domOutput = spanish[userInput];
+    const userInput = inputSpot.value.toLowerCase().split(' ');
+    let domOutput = '';
+    userInput.forEach(word => {
+        if (spanish[word]) {
+            domOutput += " " + (spanish[word]);
+        } else {
+            domOutput = "We dont know a whole lot of this language";
+        }
+    })
     outputSpot.innerHTML = domOutput;
 })
 
 const allButtons = document.getElementsByClassName('butt');
 
-for(let i=0; i<allButtons; i++){
+for (let i = 0; i < allButtons; i++) {
     allButtons[i].addEventListener('click', (e) => {
-    const userInput = inputSpot.value.toLowerCase();
-    let domOutput = '';
-    if (e.target.id === 'chineseButt' ){
-        domOutput = chineseButt[userInput];
-    } else if (e.target.id === 'french-btn'){
-        domOutput = frenchButt[userInput];
-    } else {
-        domOutput = spanishButt[userInput];
-    }
+        const userInput = inputSpot.value.toLowerCase();
+        let domOutput = '';
+        if (e.target.id === 'chineseButt') {
+            domOutput = chineseButt[userInput];
+        } else if (e.target.id === 'french-btn') {
+            domOutput = frenchButt[userInput];
+        } else {
+            domOutput = spanishButt[userInput];
+        }
         output.innerHTML = domOutput;
-  })
+    })
 }
 
 
